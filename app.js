572810234240dd643c8ce9ee0bf4a83d6070d1df
SambhavKarn <111695740+SambhavKarn@users.noreply.github.com>
2024-07-16 23:14:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 // const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+console.log('MAP_TOKEN:', process.env.MAP_TOKEN);
+
+const mapboxClient = require('@mapbox/mapbox-sdk/services/geocoding')({ accessToken: process.env.MAP_TOKEN });
 
 const dbUrl = process.env.ATLASDB_URL;
 
